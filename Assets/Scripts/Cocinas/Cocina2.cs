0d95c9120598668286c1b0fe3240d48579f7163c
IgTapia02 <igtapia001@gmail.com>
2022-11-23 21:05:34 +0100

    no funciona

--- Assets/Scripts/Cocinas/Cocina2.cs
+++ Assets/Scripts/Cocinas/Cocina2.cs
@@ -8,10 +8,11 @@ public class Cocina2 : MonoBehaviour
     GameObject Contador;
 
     bool colision;
-    bool c1, c2, c3;
     Player player;
+    public int pedido;
     int ncocina;
 
+    [SerializeField]
     GameObject[] cocinas = new GameObject[3];
     void Start()
     {
@@ -23,43 +24,104 @@ public class Cocina2 : MonoBehaviour
     {
         if (colision == true)
         {
-            if (Input.GetKeyDown(player.dejar))
+
+            if(Input.GetKeyDown(player.dejar))
             {
-                if (player.Comandas[0] != 0 && ncocina<3)
+                if(player.Comandas[0] != 0)
                 {
-                    cocinas[ncocina] = Instantiate(Contador);
-                    if(ncocina == 0)
+                    if(ncocina < 3)
                     {
-                        c1 = true;
+                        if(player.Comandas[0]!=1)
+                        {
+                            pedido = player.Comandas[0];
+                            player.Comandas[0] = 0;
+                            player.Comandas[0] = player.Comandas[1];
+                            player.Comandas[1] = player.Comandas[2];
+                            player.Comandas[2] = 0;
+                            player.numcomandas--;
+
+
+                            cocinas[ncocina] = Instantiate(Contador);
+                            ncocina++;
+
+                        }else if (player.Comandas[1] != 1 && player.Comandas[1] != 0)
+                        {
+                            pedido = player.Comandas[1];
+                            player.Comandas[1] = 0;
+                            player.Comandas[1] = player.Comandas[2];
+                            player.Comandas[2] = 0;
+                            player.numcomandas--;
+
+
+                            cocinas[ncocina] = Instantiate(Contador);
+                            ncocina++;
+
+                        }else if (player.Comandas[2] != 1 && player.Comandas[2] != 0)
+                        {
+                            pedido = player.Comandas[2];
+                            player.Comandas[2] = 0;
+                            player.numcomandas--;
+
+
+                            cocinas[ncocina] = Instantiate(Contador);
+                            ncocina++;
+
+                        }
+                        if (ncocina == 1)
+                        {
+                            cocinas[0].transform.position = new Vector3(8.73f, 4.19f, 1.26f);
+                        }
+                        else if (ncocina == 2)
+                        {
+                            cocinas[1].transform.position = new Vector3(8.73f, 4.19f, 0.24f);
+                        }
+                        else if (ncocina == 3)
+                        {
+                            cocinas[2].transform.position = new Vector3(8.73f, 4.19f, -0.94f);
+                        }
+
+
                     }
-                    if (ncocina == 1)
-                    {
-                        c2 = true;
-                    }
-                    if (ncocina == 2)
-                    {
-                        c3 = true;
-                    }
-                    ncocina++;
                 }
             }
+            
             if (Input.GetKeyDown(player.coger))
             {
-                if(cocinas[0].GetComponent<PedidosCocina>().listo == true && player.Inventario[0] == 0)
+                if(player.Inventario[0]==0)
                 {
-                    player.Coger(cocinas[0].GetComponent<PedidosCocina>().pedido);
-                    ncocina--;
-                    Destroy(cocinas[0]);
-                    cocinas[0] = cocinas[1];
-                    cocinas[1] = cocinas[2];
-                    Destroy(cocinas[2]);
+                    if(cocinas[0].GetComponent<PedidosCocina>().listo == true)
+                    {
+                        player.Coger(cocinas[0].GetComponent<PedidosCocina>().pedido);
+                        if(ncocina==1)
+                        {
+                            cocinas[0].GetComponent<PedidosCocina>().recogido = true;
+                        }
+                        else if (ncocina == 2)
+                        {
+                            cocinas[0].GetComponent<PedidosCocina>().recogido = true;
+                            cocinas[0] = cocinas[1];
+                            Destroy(cocinas[1]);
+
+                            cocinas[0].transform.position = new Vector3(8.73f, 4.19f, 1.26f);
+
+                        }
+                        else if(ncocina == 2)
+                        {
+                            cocinas[0].GetComponent<PedidosCocina>().recogido = true;
+                            cocinas[0] = cocinas[1];
+                            cocinas[1] = cocinas[2];
+                            Destroy(cocinas[2]);
+                            
+
+                            cocinas[0].transform.position = new Vector3(8.73f, 4.19f, 1.26f);
+                            cocinas[1].transform.position = new Vector3(8.73f, 4.19f, 0.24f);
+                        }
+                        ncocina--;
+                    }
                 }
 
             }
         }
-        cocinas[0].transform.position = new Vector3(8.73f, 4.19f, 1.26f);
-        cocinas[1].transform.position = new Vector3(8.73f, 4.19f, 0.24f);
-        cocinas[2].transform.position = new Vector3(8.73f, 4.19f, -0.94f);
     }
     private void OnTriggerEnter(Collider other)
     {
