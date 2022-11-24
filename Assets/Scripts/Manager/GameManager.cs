6a69e1b838b774a7de85a0a829a3b71128f54645
ntapia02 <Igtapia001@gmail.com>
2022-11-24 13:41:08 +0100

    hola

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using TMPro;
 [System.Serializable]
 
@@ -9,6 +11,8 @@ public class GameManager : MonoBehaviour
     [Header("Precios comida")]
     [SerializeField]
     int zumos,comidas;
+
+    GameData data;
 
     public GameData gameData;
     //public int dineroPJ = 0;
@@ -19,6 +23,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        gameData.dineroPJ = 0;
         gameData.dia = 1;
         gameData.semana = 1;
         DontDestroyOnLoad(this.gameObject);
@@ -40,6 +45,28 @@ public class GameManager : MonoBehaviour
         if(pedido == 2 || pedido == 3 || pedido == 4 || pedido == 5)
         {
             gameData.dineroPJ += comidas;
+        }
+    }
+    public void Save()
+    {
+        data = new GameData();
+        data.dia = GameManager.current.gameData.dia;
+        data.semana = GameManager.current.gameData.dia;
+        data.dineroPJ = GameManager.current.gameData.dia;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, data);
+        file.Close();
+    }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            data = (GameData)bf.Deserialize(file);
+            GameManager.current.gameData = data;
+            file.Close();
         }
     }
 }
