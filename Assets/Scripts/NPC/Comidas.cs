92ae483bf4481b239c4ea729c1d1b50bbc7ac077
IgTapia02 <igtapia001@gmail.com>
2022-12-06 23:03:29 +0100

    ma

--- Assets/Scripts/NPC/Comidas.cs
+++ Assets/Scripts/NPC/Comidas.cs
@@ -8,6 +8,10 @@ public class Comidas : MonoBehaviour
     SpriteRenderer spriterender;
     public bool atendido;
     public bool mitadTiempo;
+    public bool comiendo;
+
+    int numcomiendo;
+    float tiempo;
 
     [SerializeField]
     Sprite hamburgesa;
@@ -25,48 +29,101 @@ public class Comidas : MonoBehaviour
     Sprite Esperando1;
     [SerializeField]
     Sprite Esperando2;
+    [SerializeField]
+    Sprite comiendo1;
+    [SerializeField]
+    Sprite comiendo2;
+    [SerializeField]
+    Sprite comiendo3;
 
     void Start()
     {
         spriterender = GetComponent<SpriteRenderer>();
         atendido = false;
         mitadTiempo = false;
+        comiendo = false;
+        numcomiendo = 1;
 
     }
     void Update()
     {
-        if (atendido == true)
+        Debug.Log(numcomiendo);
+        Debug.Log(tiempo);
+        if (numcomiendo == 0)
         {
-            switch (pedido)
+            numcomiendo = 1;
+        }
+
+        if (comiendo == true)
+        {
+            tiempo += Time.deltaTime;
+            if (tiempo >= 0.3f)
             {
-                case 6:
-                    spriterender.sprite = paella;
-                    break;
-                case 5:
-                    spriterender.sprite = hamburgesa;
-                    break;
-                case 4:
-                    spriterender.sprite = pollo;
-                    break;
-                case 3:
-                    spriterender.sprite = tortilla;
+                switch (numcomiendo)
+                {
+                    case 1:
+                        numcomiendo = 2;
+                        break;
+                    case 2:
+                        numcomiendo = 3;
+                        break;
+                    case 3:
+                        numcomiendo = 1;
+                        break;
+                }
+                tiempo = 0;
+            }
+
+
+            switch (numcomiendo)
+            {
+                case 1:
+                    spriterender.sprite = comiendo1;
                     break;
                 case 2:
-                    spriterender.sprite = huevosPatata;
+                    spriterender.sprite = comiendo2;
                     break;
-                case 1:
-                    spriterender.sprite = zumo;
+                case 3:
+                    spriterender.sprite = comiendo3;
                     break;
             }
-        }else
+        }
+        else
         {
-            if(mitadTiempo == true)
+            if (atendido == true)
             {
-                spriterender.sprite =Esperando2;
+                switch (pedido)
+                {
+                    case 6:
+                        spriterender.sprite = paella;
+                        break;
+                    case 5:
+                        spriterender.sprite = hamburgesa;
+                        break;
+                    case 4:
+                        spriterender.sprite = pollo;
+                        break;
+                    case 3:
+                        spriterender.sprite = tortilla;
+                        break;
+                    case 2:
+                        spriterender.sprite = huevosPatata;
+                        break;
+                    case 1:
+                        spriterender.sprite = zumo;
+                        break;
+                }
             }
             else
             {
-                spriterender.sprite = Esperando1;
+                if (mitadTiempo == true)
+                {
+                    spriterender.sprite = Esperando2;
+                }
+                else
+                {
+                    spriterender.sprite = Esperando1;
+                }
             }
         }
     }
@@ -74,7 +131,7 @@ public class Comidas : MonoBehaviour
     public int EstablecerPedido(int pedidoNPC)
     {
         pedido = pedidoNPC;
-       
+
         return pedido;
     }
 
